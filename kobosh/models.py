bab14f8d6f11334d9152caff41aca08a7f3ffb2d
emmystark <nkereuwememmanuel03@gmail.com>
2025-05-10 13:37:10 +0100

    fixed_home_pgae working on admin page

--- kobosh/models.py
+++ kobosh/models.py
@@ -30,6 +30,12 @@ class Category(models.Model):
 
     slug = models.SlugField(max_length=200,
                             unique=True)
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category_detail', args=[self.slug])
 
     class Meta:
         ordering = ['name']
